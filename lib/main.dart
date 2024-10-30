fbb7ee643fe3f6f623fbf84b70756b2e7c8cd2e0
luis-santos-almonte <luisreynaldosa@gmail.com>
2024-10-30 19:35:16 -0400

    style: Adding the heart icon to the button

--- lib/main.dart
+++ lib/main.dart
@@ -51,6 +51,14 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -65,6 +73,7 @@ class MyHomePage extends StatelessWidget {
                   onPressed: () {
                     appState.toggleFavorite();
                   },
+                  icon: Icon(icon),
                   label: Text('Like'),
                 ),
                 SizedBox(width: 10),
